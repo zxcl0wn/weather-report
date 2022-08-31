47010a884ca93f098817d8f05abd81b3fd30f85f
zxcgleb <m_marple1@mail.ru>
2022-08-31 12:29:01 +1000

    123

--- main_test.py
+++ main_test.py
@@ -1,66 +1,82 @@
 import requests
 from datetime import *
 from config import tg_bot_token, weather_token
-# from aiogram import Bot, types
-# from aiogram.dispatcher import Dispatcher
-# from aiogram.utils import executor
-import telebot
 from telebot import types
-bot = telebot.TeleBot("5584052547:AAFDT20KX_4QVfbEPH7Fz9kaE1uAMoVY0_Q")
+import telebot
+import pytz
+from time import *
+import sqlite3
+
+bot = telebot.TeleBot(tg_bot_token)
+
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Напиши название города и я отправлю информацию о погоде в этом городе!")
-
-# @bot.message_handler(content_types=["Да"])
-# def yes_answer(message):
-#     await message.reply(f"")
-
-@bot.message_handler()
-def get_weather(message):
-    try:
-        r = requests.get(
-             f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_token}&units=metric"
-        )
-        data = r.json()
-        # pprint(data)
-
-        city = data['name']
-        cur_weather = data['main']['temp']
-        humidity = data['main']['humidity']
-        pressure = data['main']['pressure']
-        wind = data['wind']['speed']
-        sunrise = datetime.fromtimestamp(data['sys']['sunrise']).strftime('%H:%M')
-        sunset = datetime.fromtimestamp(data['sys']['sunset']).strftime('%H:%M')
-        lenth_of_the_day = datetime.fromtimestamp(data['sys']['sunset']) - datetime.fromtimestamp(data['sys']['sunrise'])
-        type_of_weather = {
-            "Thunderstorm": 'Гроза \U0001F329',
-            "Drizzle": 'Морось \U0001F4A7',
-            "Rain":  'Дождь \U00002614',
-            "Snow": 'Снег \U00002744',
-            "Clouds": 'Облачно \U000026C5',
-            "Clear": 'Ясно \U00002600',
-            "Smoke": 'Туман \U0001F32B'
-        }
-        weather_discription = data['weather'][0]['main']
-        if weather_discription in type_of_weather:
-            wd = type_of_weather[weather_discription]
-        else:
-            wd = "FUCK YOU"
-        # f"Сегодня: {datetime.now().strftime('%Y/%m/%d %H:%M')}"
-        bot.send_message(message.chat.id, f"\nПогода в городе {city}"
-              f"\nТемпература: {cur_weather} C°"
-              f"\n{wd}"
-              f"\nВлажность: {humidity} %"
-              f"\nДавление: {pressure} мм.рт.ст"
-              f"\nВетер: {wind} м/c"
-              f"\nВремя рассвета: {sunrise}"
-              f"\nВремя заката: {sunset}"
-              f"\nПродолжительность дня: {lenth_of_the_day}")
-        bot.send_message(message.chat.id, f"Какой-то другой город?")
+    bot.send_message(message.chat.id, f"Как тебя зовут?")
 
 
-    except:
-        bot.send_message(message.chat.id, "Такого города не существует")
+
+    @bot.message_handler(content_types=['text'])
+    def greeting(message):
+        global mes
+        mes = message.text
+        bot.send_message(message.chat.id, f"Привет, {mes}!")
+
+        @bot.message_handler(content_types=['text'])
+        def get_weather(message):
+            try:
+                r = requests.get(
+                    f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_token}&units=metric&lang=ru"
+                )
+                data = r.json()
+
+                city = data['name']
+                cur_weather = data['main']['temp']
+                humidity = data['main']['humidity']
+                pressure = data['main']['pressure']
+                wind = data['wind']['speed']
+                sunrise = datetime.fromtimestamp(data['timezone'] + data['sys']['sunrise']).astimezone(
+                    pytz.timezone('Etc/GMT')).strftime('%H:%M')
+                sunset = datetime.fromtimestamp(data['timezone'] + data['sys']['sunset']).astimezone(
+                    pytz.timezone('Etc/GMT')).strftime('%H:%M')
+                type_of_weather = {
+                    "Thunderstorm": 'Гроза \U0001F329',
+                    "Drizzle": 'Морось \U0001F4A7',
+                    "Rain": 'Дождь \U00002614',
+                    "Snow": 'Снег \U00002744',
+                    "Clouds": 'Облачно \U00002601',
+                    "Clear": 'Ясно \U00002600',
+                    "Smoke": 'Туман \U0001F32B',
+                    "Mist": 'Туман \U0001F32B'
+                }
+                weather_discription = data['weather'][0]['main']
+                if weather_discription in type_of_weather:
+                    wd = type_of_weather[weather_discription]
+                else:
+                    wd = "FUCK YOU"
+                bot.send_message(message.chat.id,
+                                 f"Сейчас в городе {city} {datetime.fromtimestamp(mktime(datetime.now(pytz.timezone('Etc/GMT')).timetuple()) + data['timezone']).strftime('%H:%M')}"
+                                 f"\nТемпература: {cur_weather} C°"
+                                 f"\n{wd}"
+                                 f"\nВлажность: {humidity} %"
+                                 f"\nДавление: {pressure} мм.рт.ст"
+                                 f"\nВетер: {wind} м/c"
+                                 f"\nВремя рассвета: {sunrise}"
+                                 f"\nВремя заката: {sunset}")
+
+                bot.register_next_step_handler(message, get_weather)
+                markup = types.InlineKeyboardMarkup()
+                mark_yes = types.InlineKeyboardButton("Да")
+                mark_no = types.InlineKeyboardMarkup("Нет")
+                markup.add(mark_yes, mark_no)
+
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, f"{mes}, Такого города не существует!!!")
+                bot.register_next_step_handler(message, get_weather)
+
+        bot.register_next_step_handler(message, get_weather)
+
+    bot.register_next_step_handler(message, greeting)
 
 
 
--- weather_report.py
+++ weather_report.py
@@ -21,66 +21,59 @@ def start_command(message):
         mes = message.text
         bot.send_message(message.chat.id, f"Привет, {mes}!")
 
-        @bot.message_handler(content_types=['text'])
-        def get_weather(message):
-            try:
-                r = requests.get(
-                    f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_token}&units=metric&lang=ru"
-                )
-                data = r.json()
-
-                city = data['name']
-                cur_weather = data['main']['temp']
-                humidity = data['main']['humidity']
-                pressure = data['main']['pressure']
-                wind = data['wind']['speed']
-                sunrise = datetime.fromtimestamp(data['timezone'] + data['sys']['sunrise']).astimezone(
-                    pytz.timezone('Etc/GMT')).strftime('%H:%M')
-                sunset = datetime.fromtimestamp(data['timezone'] + data['sys']['sunset']).astimezone(
-                    pytz.timezone('Etc/GMT')).strftime('%H:%M')
-                type_of_weather = {
-                    "Thunderstorm": 'Гроза \U0001F329',
-                    "Drizzle": 'Морось \U0001F4A7',
-                    "Rain": 'Дождь \U00002614',
-                    "Snow": 'Снег \U00002744',
-                    "Clouds": 'Облачно \U00002601',
-                    "Clear": 'Ясно \U00002600',
-                    "Smoke": 'Туман \U0001F32B',
-                    "Mist": 'Туман \U0001F32B'
-                }
-                weather_discription = data['weather'][0]['main']
-                if weather_discription in type_of_weather:
-                    wd = type_of_weather[weather_discription]
-                else:
-                    wd = "FUCK YOU"
-                bot.send_message(message.chat.id,
-                                 f"Сейчас в городе {city} {datetime.fromtimestamp(mktime(datetime.now(pytz.timezone('Etc/GMT')).timetuple()) + data['timezone']).strftime('%H:%M')}"
-                                 f"\nТемпература: {cur_weather} C°"
-                                 f"\n{wd}"
-                                 f"\nВлажность: {humidity} %"
-                                 f"\nДавление: {pressure} мм.рт.ст"
-                                 f"\nВетер: {wind} м/c"
-                                 f"\nВремя рассвета: {sunrise}"
-                                 f"\nВремя заката: {sunset}")
-
-                bot.register_next_step_handler(message, get_weather)
-                markup = types.InlineKeyboardMarkup()
-                mark_yes = types.InlineKeyboardButton("Да")
-                mark_no = types.InlineKeyboardMarkup("Нет")
-                markup.add(mark_yes, mark_no)
-
-            except Exception as ex:
-                print(ex)
-                bot.send_message(message.chat.id, f"{mes}, Такого города не существует!!!")
-                bot.register_next_step_handler(message, get_weather)
-
-        bot.register_next_step_handler(message, get_weather)
-
     bot.register_next_step_handler(message, greeting)
 
 
 
+    # bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Напиши название города и я отправлю информацию о погоде в этом городе!")
 
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    try:
+        r = requests.get(
+             f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_token}&units=metric&lang=ru"
+        )
+        data = r.json()
 
+        city = data['name']
+        cur_weather = data['main']['temp']
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        wind = data['wind']['speed']
+        sunrise = datetime.fromtimestamp(data['timezone'] + data['sys']['sunrise']).astimezone(pytz.timezone('Etc/GMT')).strftime('%H:%M')
+        sunset = datetime.fromtimestamp(data['timezone'] + data['sys']['sunset']).astimezone(pytz.timezone('Etc/GMT')).strftime('%H:%M')
+        type_of_weather = {
+            "Thunderstorm": 'Гроза \U0001F329',
+            "Drizzle": 'Морось \U0001F4A7',
+            "Rain":  'Дождь \U00002614',
+            "Snow": 'Снег \U00002744',
+            "Clouds": 'Облачно \U00002601',
+            "Clear": 'Ясно \U00002600',
+            "Smoke": 'Туман \U0001F32B',
+            "Mist": 'Туман \U0001F32B'
+        }
+        weather_discription = data['weather'][0]['main']
+        if weather_discription in type_of_weather:
+            wd = type_of_weather[weather_discription]
+        else:
+            wd = "FUCK YOU"
+        bot.send_message(message.chat.id,
+              f"Сейчас в городе {city} {datetime.fromtimestamp(mktime(datetime.now(pytz.timezone('Etc/GMT')).timetuple()) + data['timezone']).strftime('%H:%M')}"
+              f"\nТемпература: {cur_weather} C°"
+              f"\n{wd}"
+              f"\nВлажность: {humidity} %"
+              f"\nДавление: {pressure} мм.рт.ст"
+              f"\nВетер: {wind} м/c"
+              f"\nВремя рассвета: {sunrise}"
+              f"\nВремя заката: {sunset}")
+
+        markup = types.InlineKeyboardMarkup()
+        mark_yes = types.InlineKeyboardButton("Да")
+        mark_no = types.InlineKeyboardMarkup("Нет")
+        markup.add(mark_yes, mark_no)
+
+    except Exception as ex:
+        print(ex)
+        bot.send_message(message.chat.id, f"{mes}, Такого города не существует!!!")
 
 bot.polling(none_stop=True)
